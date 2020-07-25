d823b93ad9e83d23df9b9ac6a5d7b9ab93221572
Muhammad Daffa Bagus Muttaqin <63336347+daffabagus031@users.noreply.github.com>
2020-07-25 10:15:42 +0700

    Update experience.blade.php

--- resources/views/experience.blade.php
+++ resources/views/experience.blade.php
@@ -41,6 +41,9 @@
                         </p>
                     </div>
                 </div>
+
+            </div>
+            <div class="card-group">
                 <div class="card pt-3" style="width: 18rem;">
                     <img class="card-img-top ml-auto mr-auto" src="{{ ('img/sem.jpeg') }}" alt="Card image cap"
                         style="width: 18rem;">
@@ -51,8 +54,6 @@
                         </p>
                     </div>
                 </div>
-            </div>
-            <div class="card-group">
                 <div class="card pt-3" style="width: 18rem;">
                     <img class="card-img-top ml-auto mr-auto" src="{{ ('img/s.png') }}" alt="Card image cap"
                         style="width: 18rem;">
@@ -66,6 +67,41 @@
                 </div>
             </div>
             <br>
+            <h1>Pemahaman</h1>
+            <p>Pemahaman saya dalam bahasa pemrograman kira-kira seperti progres bar di bawah ini:</p>
+            <h4>Front End</h4>
+            HTML <i class="fa fa-html5" aria-hidden="true"></i>
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: 85%"
+                    aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">85%</div>
+            </div>
+            CSS <i class="fa fa-css3" aria-hidden="true"></i>
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: 70%"
+                    aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">70%</div>
+            </div>
+            JavaScript
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: 80%"
+                    aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">80%</div>
+            </div>
+            <br>
+            <h4>Back End</h4>
+            Java
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: 50%"
+                    aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">65%</div>
+            </div>
+            PHP
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped bg-warning" role="progressbar" style="width: 40%"
+                    aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">40%</div>
+            </div>
+            Python
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped bg-danger" role="progressbar" style="width: 15%"
+                    aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">15%</div>
+            </div>
         </div>
     </div>
 </div>
